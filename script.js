f3714f40bc2481883006f95e1dd837865a377d4e
Michael Seutin <frenchmike@gmail.com>
2023-07-15 02:52:49 -0700

    did some work

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const qrImage = document.querySelector('.qr-img')
 const cardResult = document.querySelector('.result')
 
 const displayQrCode = (url) => {
-    cardResult.classList.remove('dimmed')
-    qrImage.src = url 
-
+       qrImage.onload = () => {
+         cardResult.classList.remove("dimmed");
+       };
+      qrImage.src = url 
 }
 
 const sendQrCode = (params) => {
@@ -16,11 +17,11 @@ const sendQrCode = (params) => {
     fetch(fullUrl)
     .then(response => {
         console.log(response);
-        if(response.status !== 200){
-            alert('error')
-            return
-        }else{
+        if(response.ok){
             displayQrCode(response.url)
+        }else{
+            alert("An error occurred. Please try again later.");
+            throw new Error("Error: " + response.status);
         }
     })
 
